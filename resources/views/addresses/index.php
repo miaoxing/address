5c6fe47199b222808833c1a74639e83fef68c7a9
twinh <twinhuang@qq.com>
2019-02-22 14:44:17 +0800

    refactoring: 迁移 jquery-load-json

--- resources/views/addresses/index.php
+++ resources/views/addresses/index.php
@@ -15,7 +15,7 @@
   require([
     'plugins/address/js/addresses',
     'plugins/app/libs/artTemplate/template.min',
-    'comps/jquery.loadJSON/index.min',
+    'plugins/app/libs/jquery.loadJSON/index.min',
     'plugins/app/libs/jquery-form/jquery.form'
   ], function (address) {
     address.indexAction({
